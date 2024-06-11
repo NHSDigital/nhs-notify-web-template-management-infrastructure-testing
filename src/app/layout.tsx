d197afb66be32620496b0fe0574a6cea6efc1f42
muhammed.salaudeen <muhammed.salaudeen1@nhs.net>
2024-06-11 14:08:17 +0100

    CCM-4886: Made corrections from MR comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './app.scss';
-import { NHSNotifyHeader } from '../molecules/header/header';
-import { NHSNotifyContainer } from './container/container';
-import { NHSNotifyFooter } from '../molecules/footer/footer';
+import { NHSNotifyHeader } from './components/molecules/header/header';
+import { NHSNotifyContainer } from './components/layouts/container/container';
+import { NHSNotifyFooter } from './components/molecules/footer/footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
