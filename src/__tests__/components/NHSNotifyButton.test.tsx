eff4d0bafc4d9df28db553e26ed29eeb3296a029
muhammed.salaudeen <muhammed.salaudeen1@nhs.net>
2024-06-13 16:38:56 +0100

    CCM-5180: Add review page with other extras - Pa11y, unit tests, update ReadMe and folder structure

---
+++ src/__tests__/components/NHSNotifyButton.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import { NHSNotifyButton } from '@/src/components/atoms/NHSNotifyButton/NHSNotifyButton';
+import { ButtonType } from '@/src/components/atoms/NHSNotifyButton/button.types';
+
+const buttonProps: ButtonType = {
+  children: 'Button text',
+};
+
+describe('Footer component', () => {
+  it('renders component correctly as a button', () => {
+    render(<NHSNotifyButton {...buttonProps} />);
+
+    expect(screen.getByTestId('link-button')).toBeInTheDocument();
+    expect(screen.getByTestId('link-button')).toHaveTextContent(
+      buttonProps.children?.toString() ?? 'Button text'
+    );
+  });
+
+  it('renders component correctly as a link button', () => {
+    const linkButtonProps: ButtonType = { ...buttonProps, href: '#' };
+    render(<NHSNotifyButton {...linkButtonProps} />);
+
+    expect(screen.getByTestId('link-button')).toBeInTheDocument();
+    expect(screen.getByTestId('link-button')).toHaveTextContent(
+      buttonProps.children?.toString() ?? 'Button text'
+    );
+    expect(screen.getByTestId('link-button')).toHaveAttribute('href', '#');
+  });
+});
