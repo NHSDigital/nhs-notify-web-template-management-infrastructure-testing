a5f6eaf0c2bd2b8c66430d057f23b5ce3367f693
Chris Elliott <chris.elliott17@nhs.net>
2024-06-26 15:18:16 +0100

    Remove subdomain

--- terraform/components/amplify-branch/amplify_branch_branch.tf
+++ terraform/components/amplify-branch/amplify_branch_branch.tf
@@ -13,17 +13,6 @@ data "aws_ssm_parameter" "amplify_app_id" {
   name = "/${var.project}/amplify-app/${var.amplify_app_environment}/amplify-app-id"
 }
 
-resource "aws_amplify_domain_association" "domain_association" {
-  app_id                = data.aws_ssm_parameter.amplify_app_id.value
-  domain_name           = "notify.nhs.co.uk"
-  wait_for_verification = false
-
-  sub_domain {
-    branch_name = aws_amplify_branch.branch.branch_name
-    prefix      = var.environment
-  }
-}
-
 resource "aws_amplify_webhook" "webhook" {
   app_id      = data.aws_ssm_parameter.amplify_app_id.value
   branch_name = aws_amplify_branch.branch.branch_name
